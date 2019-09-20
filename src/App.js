b5e6b7575c916389b7398302b4ec3399c50399d7
Chetan <chetan.virani@volansys.com>
2019-09-20 19:31:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "assets/scss/app.scss";
+import FunctionalComponent from "lectures/components/functionalcomponents/FunctionalComponent";
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* Functional Component */}
+
+      <FunctionalComponent />
+
+      {/* Functional Component */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/lectures/components/functionalcomponents/FunctionalComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FunctionalComponent = (props) => {
+    console.log(props)
+    return(
+        <div>
+            It is your Simple Functional Components;
+        </div>
+    )
+}
+
+export default FunctionalComponent;
